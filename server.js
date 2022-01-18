be0c8e07fa12578ab86fa5513b30a24fa67c5aac
Josh Bishop <jsh.bshp@outlook.com>
2022-01-17 22:55:37 -0500

    added strava oauth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const got = require('got');
 
 // Constants
 const PORT = process.env.PORT;
@@ -8,17 +9,28 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send(process.env.NODE_ENV);
+
+app.use(express.static('public'))
+
+app.post('/api/auth/:code', (req, res) => {
+	var result;
+	const auth_code = req.params.code;
+	got('https://www.strava.com/oauth/token', { 
+		json: true,
+		searchParams: {
+			client_id: process.env.CLIENT_ID,
+			client_secret: process.env.CLIENT_SECRET,
+			code: auth_code,
+			grant_type: "authorization_code"
+		}
+	}).then(response => {
+		result["access_token"] = response.body.access_token;
+		result["refresh_token"] = response.body.refresh_token;
+	}).catch(error => {
+  		console.log(error.response.body);
+	});
+	res.send(JSON.stringify(result));
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-
-
-
-
-
-
-

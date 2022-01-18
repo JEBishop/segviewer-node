470461f93bc8b2472ea4df31e8fd7938b8bb73e8
Josh Bishop <jsh.bshp@outlook.com>
2022-01-18 17:01:05 -0500

    backend auth, frontend segment processing

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 'use strict';
 
-const express = require('express');
-const got = require('got');
+const express = require("express");
+const got = require("got");
+const bodyParser = require("body-parser");
 
 // Constants
 const PORT = process.env.PORT;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
+const router = express.Router();
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/",router);
 
-app.post('/api/auth/:code', (req, res) => {
-	var result;
+router.get("/api/auth/:code/:grantType", async function(req, res) {
+	var result = {
+		access_token: "",
+		refresh_token: ""
+	};
+	  
+	var auth_type = "code"
 	const auth_code = req.params.code;
-	got('https://www.strava.com/oauth/token', { 
+	const grant_type = req.params.grantType;
+	if(grant_type === "refresh_token") {
+		auth_type = grant_type;
+	}
+	
+	await got.post(`https://www.strava.com/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&${auth_type}=${auth_code}&grant_type=${grant_type}`, { 
 		json: true,
-		searchParams: {
-			client_id: process.env.CLIENT_ID,
-			client_secret: process.env.CLIENT_SECRET,
-			code: auth_code,
-			grant_type: "authorization_code"
-		}
 	}).then(response => {
-		result["access_token"] = response.body.access_token;
-		result["refresh_token"] = response.body.refresh_token;
+		result.access_token = response.body.access_token;
+		result.refresh_token = response.body.refresh_token;
 	}).catch(error => {
-  		console.log(error.response.body);
+  		console.log("error: " + error);
 	});
+	
 	res.send(JSON.stringify(result));
 });
 
